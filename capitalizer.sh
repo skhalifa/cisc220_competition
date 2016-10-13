dbee332d876ad95347d4c065d0e813ec48a9cf73
Kevin Zuern <kevin@kevinzuern.com>
2016-10-13 11:24:25 -0400

    capitalizer problem

---
+++ capitalizer.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+read -p "Enter a string: " input
+
+function changeToUppercase() {
+    B=( $1 )
+    capitalized="${B[@]^}"
+    echo -n $capitalized
+    echo " changed to uppercase"
+}
+
+for word in awk $input; do
+    character=${word:0:1}
+
+    if [[ $character =~ [a-z] ]]; then
+	changeToUppercase $word
+    elif [[ $character =~ [A-Z] ]]; then
+	echo -n $word
+	echo " was already uppercase"
+    else
+	echo " doesn't start with a letter"
+    fi
+done
